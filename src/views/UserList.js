5c46a705f654861e4f43cd49ccabd5bb881453e1
Afonso Machado <afonso777@hotmail.com>
2020-11-01 21:01:10 -0300

    list item!

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
+import {Avatar, ListItem} from 'react-native-elements';
+import users from '../data/users';
 
 export default (props) => {
-  return <Text>User List</Text>;
+  // função para renderizar os usuarios
+  function getUserItem({item: user}) {
+    // listando os itens
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}>
+        <Avatar source={{uri: user.avatarUrl}} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        // dados usados
+        data={users}
+        keyExtractor={(user) => user.id.toString()}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
